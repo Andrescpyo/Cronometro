756fbbdfc86e91db142f3dc67b3a62a839809e61
Andrescpyo <142014854+Andrescpyo@users.noreply.github.com>
2023-08-27 22:21:37 -0500

    cronometro terminado :3

--- principal.py
+++ principal.py
@@ -2,12 +2,10 @@ import time
 import os
 from cronometro import Cronometro
 
-hora = input("Ingrese la hora en formato HH:MM:SS: ")
+hora = input("\nIngrese la hora en formato HH:MM:SS: ")
 
 horas, minutos, segundos = map(int, hora.split(":"))
 total_segundos = horas * 3600 + minutos * 60 + segundos + 1
-
-print("La hora ingresada en segundos es:", total_segundos)
 
 c = Cronometro()
 
@@ -16,7 +14,19 @@ for i in range(total_segundos):
     hora_actual = "{:02d}".format(c.hora.valor)
     minuto_actual = "{:02d}".format(c.minuto.valor)
     segundo_actual = "{:02d}".format(c.segundo.valor)
-    print(f"{hora_actual}:{minuto_actual}:{segundo_actual}")
-    time.sleep(1)
-    os.system('cls' if os.name == "nt" else  'clear')
+
+    cronometro_str = f"\r{hora_actual} : {minuto_actual} : {segundo_actual}"
+    print("\n", cronometro_str, end="")
+
+    # Hacemos que los dos puntos parpadeen
+    if i % 2 == 0:  
+        cronometro_str = f"\r{hora_actual}   {minuto_actual}   {segundo_actual}"
+    print(cronometro_str, end="")
+
     c.avanzar()
+    time.sleep(1)
+
+    #os.system  hace que la siguiente impresión sobrescriba la línea anterior.
+    os.system('cls' if os.name == "nt" else  'clear')
+    
+print("\n¡Tiempo terminado! :3\n")
